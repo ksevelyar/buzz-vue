3c5f76046a841fce2387544f3e4c129676c5a027
Sergey Zubkov <ksevelyar@protonmail.com>
2024-10-13 10:56:44 +0300

    add handle to messages

--- src/App.vue
+++ src/App.vue
@@ -63,40 +63,62 @@ joinTopic("room:lobby")
 </script>
 
 <template lang="pug">
-.messages
-  .message(v-for="message in messages") {{ message.body }}
+.grid-container
+  .grid-item.input-wrapper
+    input.input(v-model="inputText" @keydown.enter="sendMessage" autofocus)
 
-input.input(
-  v-model="inputText"
-  @keydown.enter="sendMessage"
-)
+  .grid-item.messages
+    .message(v-for="message in messages") 
+      span.handle {{ message.handle }}:
+      span {{ message.body }}
 
-ul.users
-  li.user(v-for="user in users") {{ user }}
+  .grid-item.users
+    .user(v-for="user in users") {{ user }}
 </template>
 
 <style>
 .app
-  display: flex
-  align-items: center
-  display: flex
-  justify-content: center
-  min-height: 100vh
-  flex-direction: column
-  gap: 10px
   background: #d4d4d4
 
 .input
   border-radius: 4px
   border: 1px solid #ddd
   padding: 10px
-  width: 50%
   outline: none
-  max-width: 80ch
+  width: 100%
 
 body
   margin: 0
 
 input, textarea, select
   box-sizing: border-box
+
+.grid-container
+  display: grid
+  grid-template-columns: auto minmax(auto, 30ch)
+  grid-template-rows: auto 40px
+  gap: 0px
+  height: 100vh
+  width: 100%
+
+.grid-item
+  border: 1px solid #ccc
+
+.input-wrapper
+  grid-column: 1
+  grid-row: 2
+
+.messages
+  padding: 20px
+  grid-column: 1
+  grid-row: 1
+
+.users
+  padding: 20px
+  grid-column: 2
+  grid-row: 1 / 2
+
+.handle
+  margin-right: 1ch
+  color: gray
 </style>
